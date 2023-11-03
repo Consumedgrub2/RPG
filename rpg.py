9faf17268e08d9d2f496edb7c6e12a1337a57141
Consumedgrub2 <maherscp0@gmail.com>
2023-11-02 20:42:45 -0700

    Clean up

--- rpg.py
+++ rpg.py
@@ -1,20 +1,3 @@
 import random
 import io
 import os
-import pathlib
-
-test = 5
-test2 = "This is the string!"
-
-if test == 5:
-    print('hi')
-
-def helloWorld():
-    print("Hello World! \nSup Mark!")
-    print(str(6 + test) + " " + str(test2))
-
-helloWorld()
-
-someFile = open(os.getcwd() + "/test.txt", "w")
-someFile.write("Hello there!")
-someFile.flush()
