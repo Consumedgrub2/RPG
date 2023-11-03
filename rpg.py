eeb0cf2502bcc971d5d66986265d527a2a17d27d
Specabyss <norrismark520@gmail.com>
2023-11-02 20:35:58 -0700

    add an if

--- rpg.py
+++ rpg.py
@@ -6,6 +6,9 @@ import pathlib
 test = 5
 test2 = "This is the string!"
 
+if test == 5:
+    print('hi')
+
 def helloWorld():
     print("Hello World! \nSup Mark!")
     print(str(6 + test) + " " + str(test2))
